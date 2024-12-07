72039cdd4a9718fcdca71aae650f597db65670a9
Pimping . <auagfuiawg@mail.ru>
2024-12-08 00:29:24 +0200

    Intermediate save TODO: FINISH SWAPING CARDS

--- Assets/Scripts/Containers/TagsContainer.cs
+++ Assets/Scripts/Containers/TagsContainer.cs
@@ -3,6 +3,7 @@
     public static class TagsContainer
     {
         public const string PLAYERCARDSLOT = "PlayerCardSlot";
+        public const string PLAYERCARDITEMMODEL = "PlayerCardItemModel";
         public const string INTERACTABLE = "Interactable";
     }
 }
