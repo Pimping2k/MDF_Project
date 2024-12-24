2a68f67ba265d7d0d6641b761da23509df9000a3
AB <64165784+ABPaladin@users.noreply.github.com>
2024-12-24 19:44:38 +0200

    basic interface (just front)

---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene"); // Функция должна быть public
+    }
+
+    public void OpenOptions()
+    {
+        Debug.Log("Options menu opened"); // Добавьте код для вызова окна опций
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game exited");
+    }
+}
+
+
